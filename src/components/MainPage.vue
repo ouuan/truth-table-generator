6de7f054698ef2fec8cb08f2dd1b477a634149eb
Yufan You <ouuansteve@gmail.com>
2021-10-21 10:13:11 +0800

    refactor: add component InputTips

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -25,37 +25,7 @@
           title="输入"
           name="input"
         >
-          <n-p>
-            <n-space justify="space-between">
-              <n-ul>
-                <n-li>
-                  运算符的种类，表示方法，以及优先级顺序：
-                  <n-ol v-if="showAllOperators">
-                    <n-li>非: ¬, !, ！, ~</n-li>
-                    <n-li>与: ∧, &amp;</n-li>
-                    <n-li>与非: ↑, ⊼</n-li>
-                    <n-li>异或: ⊕, ^, ⊻</n-li>
-                    <n-li>或: ∨, |, ｜, v</n-li>
-                    <n-li>或非: ↓, ⊽</n-li>
-                    <n-li>蕴含（右结合）: →, &gt;</n-li>
-                    <n-li>被蕴含: ←, &lt;</n-li>
-                    <n-li>等价: ↔, ⟷, =</n-li>
-                  </n-ol>
-                  <span v-else>
-                    非: !，与: &amp;，或: |，蕴含: &gt;，等价: =
-                  </span>
-                </n-li>
-                <n-li>T/F 表示 true/false</n-li>
-                <n-li>命题变项用除了 T/F 的单个大写字母表示</n-li>
-              </n-ul>
-              <n-form-item
-                label="显示更多"
-                label-placement="left"
-              >
-                <n-switch v-model:value="showAllOperators" />
-              </n-form-item>
-            </n-space>
-          </n-p>
+          <input-tips />
           <n-form-item
             label="输入逻辑表达式"
             :validation-status="validationStatus"
@@ -142,13 +112,9 @@ import {
   NDataTable,
   NFormItem,
   NInput,
-  NLi,
-  NOl,
   NP,
   NPageHeader,
   NSpace,
-  NSwitch,
-  NUl,
   useThemeVars,
 } from 'naive-ui';
 
@@ -159,6 +125,7 @@ import { getTable, Column } from '~/core/getTable';
 import Step from '~/types/step';
 
 import AboutSection from './AboutSection.vue';
+import InputTips from './InputTips.vue';
 import NormalForms from './NormalForms.vue';
 import SimplificationSteps from './SimplificationSteps.vue';
 import WangHaoProof from './WangHaoProof.vue';
@@ -174,7 +141,6 @@ const atoms = shallowRef<string[]>([]);
 const truths = shallowRef<boolean[]>([]);
 const ok = ref(false);
 const rawRoot = shallowRef<AstNode | null>(null);
-const showAllOperators = ref(false);
 
 function addStep(step: Step) {
   steps.value.push(step);
